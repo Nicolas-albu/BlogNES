5b7028e6ef019721893e5063e4d7535e1bfee654
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-10-09 09:11:39 -0300

    Controllers das rotas adicionado

---
+++ blognes/controllers/__init__.py
@@ -0,0 +1,15 @@
+from .controllers import (
+    get_login,
+    get_profile,
+    post_login,
+    post_register,
+    update_user,
+)
+
+__all__ = [
+    "get_login",
+    "post_login",
+    "update_user",
+    "get_profile",
+    "post_register",
+]
---
+++ blognes/controllers/controllers.py
@@ -0,0 +1,64 @@
+from typing import Any, Dict
+
+from flask import flash, make_response, redirect, render_template, url_for
+
+from ..core import Authenticator, User, generate_token
+from ..core.config import __COOKIE_NAME__
+
+auth = Authenticator()
+
+
+def post_register(form: Dict[str, Any], /):
+    global auth
+
+    cookie = generate_token(form["password"])
+    user = User(**form, cookie=cookie)
+
+    auth.add_user(user)
+
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie(__COOKIE_NAME__, cookie)
+
+    return response
+
+
+def get_login(*, cookies: Dict[str, str]):
+    if __COOKIE_NAME__ in cookies:
+        return redirect(url_for("index"))
+
+    return render_template("login.html")
+
+
+def post_login(*, username: str, password: str):
+    global auth
+
+    if auth.is_user(username, password):
+        cookie = generate_token(password)
+        auth.set_session(username, password, cookie=cookie)
+
+        response = make_response(redirect(url_for("index")))
+        response.set_cookie(__COOKIE_NAME__, cookie)
+        return response
+
+    flash("Nome de usuário e/ou senha incorreto", "error")
+    return redirect(url_for("login"))
+
+
+def get_profile(*, cookies: Dict[str, str]):
+    if __COOKIE_NAME__ not in cookies:
+        return redirect(url_for("login"))
+
+    cookie = cookies[__COOKIE_NAME__]
+
+    if _user := auth.get_user(cookie=cookie):
+        return render_template("profile.html", user=_user.to_dict())
+
+    return redirect(url_for("login"))
+
+
+def update_user(form: Dict[str, Any], /, *, cookies: Dict[str, str]):
+    if _cookie := cookies.get(__COOKIE_NAME__):
+        auth.update_user(form, cookie=_cookie)
+        return redirect(url_for("profile"))
+
+    return redirect(url_for("login"))
