d52d0257329f96d839813b043ecb0f31ea1c3a87
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-10-09 09:04:29 -0300

    Arquivo de configuração do core

---
+++ blognes/core/config.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+__COOKIE_NAME__ = "krerley_cookie"
+__SESSION_ADMIN__ = {
+    "name": "admin",
+    "password": "$2b$15$TFc0tD54emCn4TTKhEeeWuJ47hZMfQyhOzI8laNscXOsp4fpQFKlG",
+    "cep": "0000-000",
+    "home_address": "NES",
+    "email": "nes@ufal.edu.br",
+    "telephone": "(00) 00000-0000",
+    "city": "NES",
+    "cookie": None,
+    "date_of_birth": date(2023, 1, 1),
+}
+
+__all__ = [
+    "__COOKIE_NAME__",
+    "__SESSION_ADMIN__",
+]
